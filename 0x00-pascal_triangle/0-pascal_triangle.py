4d0f15edaa3c9cae8994f2820265ab32feec18ef
richard579 <richardbeng358@outlook.com>
2024-01-03 14:12:34 +0000

    0. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-o-pascal_triangle
+0-pascal_triangle
 """
 
 
@@ -18,7 +18,7 @@ def pascal_triangle(n):
         temp = [1]
         for j in range(len(k[i - 1]) - 1):
             curr = k[i - 1]
-            temp.append(k(i - 1][j] + k[i - 1][j + 1])
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
         temp.append(1)
         k.append(temp)
     return k
