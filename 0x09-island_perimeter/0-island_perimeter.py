f1bed36f2c4a19d56b8cc61c43c13f040dbc3c29
richard579 <richardbeng358@outlook.com>
2024-03-05 11:29:06 +0000

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines island perimeter finding function"""
 
+
 def island_perimeter(grid):
     """Return the perimeter of an island
     The grid represents water by 0 and land by 1
